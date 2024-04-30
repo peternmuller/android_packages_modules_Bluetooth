25c5af1d683d0dbdabcf269a741acdcabea168c0
Satheesh Kumar Pallemoni <quic_spallemo@quicinc.com>
2024-04-30 16:31:21 +0530

    LE-Audio: Handover properly when SHO done while voice ongoing call.
    - When switch happened from BREDR to LE-A device, LE-A device becomes
      active and updated to MM audio, on which MM adds the LE-A Audio
      device and gives OnAudioDevicesadded cb after opening sessions.
    
    - On basis of Active device update MM sent upadteMetadata and start
      sent to BT Host, on which C-I-S establishment request would gets
      trigger.
    
    - But there is SCO existed still due to no classic setactive null
      called. This happens when BT app gets OnAudioDevicesAdded callback.
    
    - To handle this situation, call immediately setactive null for
      HFP when LeAudiodevice setacive called. So that it would synchronize
      both transports properly.
    
    CRs-Fixed: 3796728
    Change-Id: Ice80509346b22f1272b55f5a55978698a50e7fc3

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -2191,6 +2191,16 @@ public class LeAudioService extends ProfileService {
                 }
             }
         } */
+
+        if (!Utils.isDualModeAudioEnabled()) {
+            HeadsetService headsetService = mServiceFactory.getHeadsetService();
+            if (headsetService == null) {
+                Log.d(TAG, "There is no HFP service available");
+                return false;
+            }
+            headsetService.setActiveDevice(null);
+        }
+
         return setActiveGroupWithDevice(device, false);
     }
 
