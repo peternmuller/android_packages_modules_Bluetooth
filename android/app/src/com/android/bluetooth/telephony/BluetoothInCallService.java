796ca0db18ccc030e7de6568a505bb2a9c0950c2
Suresh Koleti <quic_skoleti@quicinc.com>
2024-07-12 18:33:56 +0530

    BT: Get appropriate call based on call state
    - Telecom can report different call states between parent
      and child calls in IMS conference calls as child calls
      will be local calls to Telecom and when conference call is
      moved to HOLD child Telcom calls state will be ACTIVE only
      which is the legacy behavior in Telecom.
    - If any clients like BT getting the ACTIVE call from Telecom
      will get child ACTIVE call even conference call is HOLD.
    - To fix this get only the parent call to get proper call
      based on call state.
    
    Change-Id: I722292f3dc2ca3ae5a4349ec294ef966c30f87fa
    CRs-Fixed: 3854971

--- android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
+++ android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -2438,7 +2438,7 @@ public class BluetoothInCallService extends InCallService {
         public BluetoothCall getCallByState(int state) {
             List<BluetoothCall> calls = getBluetoothCalls();
             for (BluetoothCall call : calls) {
-                if (state == call.getState()) {
+                if (state == call.getState() && call.getParentId() == null) {
                     return call;
                 }
             }
