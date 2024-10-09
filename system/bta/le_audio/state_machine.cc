f14e5809f31a26a85ef2d87f11b868ce9d4ccbb0
Satheesh Kumar Pallemoni <quic_spallemo@quicinc.com>
2024-10-09 20:21:25 +0530

    Le-Audio: Remove C-I-G when Bcast source added.
    - When Bcast source added Remote notifying Releasing->CodecConfigured.
      On which C-I-G not removed although C-I-Ses got disconnected.
    
    - Due to this in next iteration, Unicast stream not started and got
      timed out, leading to disconnection.
    
    - Fixed by issuing C-I-G disconnection after moving to CodecConfigured
      state.
    
    CRs-Fixed: 3938097
    Change-Id: I7c0fa06751800e059bd767e978292e77f6625cb5

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -2766,6 +2766,8 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         }
 
         cancel_watchdog_if_needed(group->group_id_);
+        ReleaseCisIds(group);
+        RemoveCigForGroup(group);
 
         state_machine_callbacks_->StatusReportCb(
             group->group_id_, GroupStreamStatus::CONFIGURED_AUTONOMOUS);
