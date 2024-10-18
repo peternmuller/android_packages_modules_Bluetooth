miyu-xu/packages-modules-Bluetooth

Enable RAS only for channel sounding.
Bug: 361816733
Test: m com.android.btservices
Change-Id: I0a9efe7e4dc7c1788643e5e7b2caf928b4660670

system/main/shim/distance_measurement_manager.cc
@@ -44,10 +44,13 @@ public:
   }
 
   void StartDistanceMeasurement(RawAddress raw_address, uint16_t interval, uint8_t method) {
+    DistanceMeasurementMethod distance_measurement_method =
+            static_cast<DistanceMeasurementMethod>(method);
     bluetooth::shim::GetDistanceMeasurementManager()->StartDistanceMeasurement(
-            bluetooth::ToGdAddress(raw_address), interval,
-            static_cast<DistanceMeasurementMethod>(method));
-    bluetooth::ras::GetRasClient()->Connect(raw_address);
+            bluetooth::ToGdAddress(raw_address), interval, distance_measurement_method);
+    if (distance_measurement_method == DistanceMeasurementMethod::METHOD_CS) {
+      bluetooth::ras::GetRasClient()->Connect(raw_address);
+    }
   }
 
   void StopDistanceMeasurement(RawAddress raw_address, uint8_t method) {

peternmuller/android_packages_modules_Bluetooth

Bug: 324185011
CRs-Fixed: 3952852
(cherry picked from commit 39a6fc84620915916ab40bc5a40c2d2a646de5b6)

system/main/shim/distance_measurement_manager.cc
@@ -60,10 +60,14 @@ public:
   }
 
   void StartDistanceMeasurement(RawAddress identity_addr, uint16_t interval, uint8_t method) {
+    DistanceMeasurementMethod distance_measurement_method =
+            static_cast<DistanceMeasurementMethod>(method);
     bluetooth::shim::GetDistanceMeasurementManager()->StartDistanceMeasurement(
             bluetooth::ToGdAddress(identity_addr), GetConnectionHandle(identity_addr), interval,
-            static_cast<DistanceMeasurementMethod>(method));
-    bluetooth::ras::GetRasClient()->Connect(identity_addr);
+            distance_measurement_method);
+    if (distance_measurement_method == DistanceMeasurementMethod::METHOD_CS) {
+      bluetooth::ras::GetRasClient()->Connect(identity_addr);
+    }
   }
 
   void StopDistanceMeasurement(RawAddress identity_addr, uint8_t method) {