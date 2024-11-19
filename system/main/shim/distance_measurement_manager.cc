miyu-xu/packages-modules-Bluetooth

RAS: Add timeout alarm for ranging data
Flag: com.android.bluetooth.flags.channel_sounding_in_stack
Bug: 324185011
Bug: 369303724
Test: m com.android.btservice
Change-Id: I5d58a38e9d71f064b1e3042d81eaaebdff4fee98

system/main/shim/distance_measurement_manager.cc
@@ -221,6 +221,12 @@ public:
             bluetooth::ToGdAddress(address), GetConnectionHandle(address), data);
   }
 
+  // Must be called from main_thread
+  void OnRemoteDataTimeout(const RawAddress& address) {
+    bluetooth::shim::GetDistanceMeasurementManager()->HandleRemoteDataTimeout(
+            bluetooth::ToGdAddress(address), GetConnectionHandle(address));
+  }
+
 private:
   ::DistanceMeasurementCallbacks* distance_measurement_callbacks_;
   static constexpr uint16_t kIllegalConnectionHandle = 0xffff;

peternmuller/android_packages_modules_Bluetooth

CRs-Fixed: 3978369
(cherry picked from commit 9c2c6aebb91b2f6905bd7be92b829a3d8cbe4436)

system/main/shim/distance_measurement_manager.cc
@@ -237,6 +237,12 @@ public:
             bluetooth::ToGdAddress(address), GetConnectionHandleAndRole(address), data);
   }
 
+  // Must be called from main_thread
+  void OnRemoteDataTimeout(const RawAddress& address) {
+    bluetooth::shim::GetDistanceMeasurementManager()->HandleRemoteDataTimeout(
+            bluetooth::ToGdAddress(address), GetConnectionHandleAndRole(address));
+  }
+
 private:
   ::DistanceMeasurementCallbacks* distance_measurement_callbacks_;
   static constexpr uint16_t kIllegalConnectionHandle = 0xffff;