miyu-xu/packages-modules-Bluetooth

Supports start CS distance measurement from peripheral
Bug: 324185011
Bug: 350777645
Test: m com.android.btservices

Change-Id: Ica050eb7b9adc3f1c85eb104f60a7091726390c0

system/bta/include/bta_ras_api.h
@@ -37,6 +37,8 @@ public:
   virtual void OnVendorSpecificReply(
           const RawAddress& address,
           const std::vector<VendorSpecificCharacteristic>& vendor_specific_reply) = 0;
+  virtual void OnRasServerGettingRequest(const RawAddress& identity_address) = 0;
+  virtual void OnRasServerDisconnected(const RawAddress& identity_address) = 0;
 };
 
 class RasServer {

system/gd/hci/distance_measurement_manager.h
@@ -19,6 +19,7 @@
 
 #include "address.h"
 #include "hal/ranging_hal.h"
+#include "hci/hci_packets.h"
 #include "module.h"
 
 namespace bluetooth {
@@ -79,17 +80,21 @@ public:
   DistanceMeasurementManager& operator=(const DistanceMeasurementManager&) = delete;
 
   void RegisterDistanceMeasurementCallbacks(DistanceMeasurementCallbacks* callbacks);
-  void StartDistanceMeasurement(const Address&, uint16_t connection_handle, uint16_t interval,
+  void StartDistanceMeasurement(const Address&, uint16_t connection_handle,
+                                hci::Role local_hci_role, uint16_t interval,
                                 DistanceMeasurementMethod method);
   void StopDistanceMeasurement(const Address& address, uint16_t connection_handle,
                                DistanceMeasurementMethod method);
-  void HandleRasConnectedEvent(
+  void HandleRasClientConnectedEvent(
           const Address& address, uint16_t connection_handle, uint16_t att_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_data);
-  void HandleRasDisconnectedEvent(const Address& address);
+  void HandleRasClientDisconnectedEvent(const Address& address);
   void HandleVendorSpecificReply(
           const Address& address, uint16_t connection_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_reply);
+  void HandleRasServerGettingRequest(const Address& address, uint16_t connection_handle,
+                                     hci::Role local_hci_role);
+  void HandleRasServerDisconnected(const Address& identity_address, uint16_t connection_handle);
   void HandleVendorSpecificReplyComplete(const Address& address, uint16_t connection_handle,
                                          bool success);
   void HandleRemoteData(const Address& address, uint16_t connection_handle,

peternmuller/android_packages_modules_Bluetooth

Flag: com.android.bluetooth.flags.channel_sounding_in_stack
CRs-Fixed: 3978369
(cherry picked from commit 5a096122ebcd9c38f5fa4e2a05c33bde5afec8c8)

system/bta/include/bta_ras_api.h
@@ -37,6 +37,8 @@ public:
   virtual void OnVendorSpecificReply(
           const RawAddress& address,
           const std::vector<VendorSpecificCharacteristic>& vendor_specific_reply) = 0;
+  virtual void OnRasServerConnected(const RawAddress& identity_address) = 0;
+  virtual void OnRasServerDisconnected(const RawAddress& identity_address) = 0;
 };
 
 class RasServer {

system/gd/hci/distance_measurement_manager.h
@@ -19,6 +19,7 @@
 
 #include "address.h"
 #include "hal/ranging_hal.h"
+#include "hci/hci_packets.h"
 #include "module.h"
 
 namespace bluetooth {
@@ -79,17 +80,21 @@ public:
   DistanceMeasurementManager& operator=(const DistanceMeasurementManager&) = delete;
 
   void RegisterDistanceMeasurementCallbacks(DistanceMeasurementCallbacks* callbacks);
-  void StartDistanceMeasurement(const Address&, uint16_t connection_handle, uint16_t interval,
+  void StartDistanceMeasurement(const Address&, uint16_t connection_handle,
+                                hci::Role local_hci_role, uint16_t interval,
                                 DistanceMeasurementMethod method);
   void StopDistanceMeasurement(const Address& address, uint16_t connection_handle,
                                DistanceMeasurementMethod method);
-  void HandleRasConnectedEvent(
+  void HandleRasClientConnectedEvent(
           const Address& address, uint16_t connection_handle, uint16_t att_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_data);
-  void HandleRasDisconnectedEvent(const Address& address);
+  void HandleRasClientDisconnectedEvent(const Address& address);
   void HandleVendorSpecificReply(
           const Address& address, uint16_t connection_handle,
           const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_reply);
+  void HandleRasServerConnected(const Address& identity_address, uint16_t connection_handle,
+                                hci::Role local_hci_role);
+  void HandleRasServerDisconnected(const Address& identity_address, uint16_t connection_handle);
   void HandleVendorSpecificReplyComplete(const Address& address, uint16_t connection_handle,
                                          bool success);
   void HandleRemoteData(const Address& address, uint16_t connection_handle,